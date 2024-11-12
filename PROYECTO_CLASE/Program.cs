c98fcd8eb07fd426d443c82dc61aa9b9cfc09255
Soyfernand00 <n00443358@upn.pe>
2024-11-12 13:39:54 -0500

    first

---
+++ PROYECTO_CLASE/Program.cs
@@ -0,0 +1,28 @@
+﻿byte opcion;
+string vol;
+
+do
+{
+    Console.WriteLine("\tMENU");
+    Console.WriteLine("Crear [1]:");
+    Console.WriteLine("Lista [2]:");
+    Console.WriteLine("Eliminar [3]:");
+    Console.WriteLine("Salir [0]:");
+
+    Console.WriteLine("Ingrese opcion: ");
+    while (!byte.TryParse(Console.ReadLine(), out opcion) || opcion > 3)
+    {
+        Console.WriteLine("Error: Ingrese una opcion valida: ");
+    }
+    switch (opcion)
+    {
+        case 0: Environment.Exit(0); break;
+        case 1: break;
+        case 2: break;
+        case 3: break;
+    }
+    Console.WriteLine("¿Volver al menú? [si]: ");
+    vol = Console.ReadLine().ToLower();
+    Console.Clear();
+}while (vol=="si");
+
