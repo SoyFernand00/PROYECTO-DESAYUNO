6659cb239feff53fb70b8019e27d10e3e99445ba
I'mFernando <n00443358@upn.pe>
2024-11-13 21:36:53 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using PROYECTO_CLASE;
+
+byte opcion;
+string vol;
+
+desayuno de=new desayuno();
+do
+{
+    Console.WriteLine("\tMENU");
+    Console.WriteLine("Crear [1]:");
+    Console.WriteLine("Lista [2]:");
+    Console.WriteLine("Eliminar [3]:");
+    Console.WriteLine("Salir [0]:");
+
+    Console.WriteLine("Ingrese opcion: ");
+    while (!byte.TryParse(Console.ReadLine(), out opcion) || opcion > 3)
+    {
+        Console.WriteLine("Error: Ingrese una opcion valida: ");
+    }
+    switch (opcion)
+    {
+        case 0: Environment.Exit(0); break;
+        case 1:
+            de.Crear();
+            break;
+        case 2:
+            de.Lista();
+            break;
+        case 3:
+            Console.Write("\nIngresar desayuno a eliminar: ");
+            string Desa = Console.ReadLine();
+            de.Eliminar1(Desa);
+            break;
+    }
+    Console.WriteLine("\n¿Volver al menú? [si]: ");
+    vol = Console.ReadLine().ToLower();
+    Console.Clear();
+}while (vol=="si");
+
---
+++ desayuno.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROYECTO_CLASE
+{
+    internal class desayuno
+    {
+        string[] Desayuno1 = new string[0];
+        int posicion = 0;
+        string seguir;
+
+
+        public void Crear()
+        {
+            do
+            {
+                string Respuesta;
+                Console.Write("\n¿Ingresa el desayuno que te gustaria? : ");
+                Respuesta = Console.ReadLine();
+                Array.Resize(ref Desayuno1, Desayuno1.Length + 1);
+                Desayuno1[posicion] = Respuesta;
+                posicion++;
+
+                Console.WriteLine("\n!!Desayuno ingresado correctamente!! ");
+                Console.WriteLine("Desea ingresar un nuevo desayuno? [s] [n]: ");
+                seguir = Console.ReadLine().ToLower();
+            } while (seguir == "s");
+
+        }
+        public void Lista()
+        {
+            Console.SetCursorPosition(10, 12);
+
+            Console.Write("Servidos: ");
+            Console.SetCursorPosition(30, 12);
+
+            Console.Write("Desayunos: ");
+            for (int i = 0; i < Desayuno1.Length; i++)
+            {
+                Console.SetCursorPosition(10, 13 + i);
+                Console.Write(i);
+                Console.SetCursorPosition(30, 13 + i);
+                Console.WriteLine(Desayuno1[i]);
+
+            }
+        }
+        public void Eliminar1(string Desa)
+        {
+            int indice = Array.IndexOf(Desayuno1, Desa);
+            if (indice != -1)
+            {
+                for (int i = indice; i < Desayuno1.Length - 1; i++)
+                {
+                    Desayuno1[i] = Desayuno1[i + 1];
+                }
+                Array.Resize(ref Desayuno1, Desayuno1.Length - 1);
+                posicion--;
+
+                Console.WriteLine("\nUn desayuno fue eliminado correctamente");
+
+            }
+            else
+            {
+                Console.WriteLine("\nNo se puede eliminar Porque no Existe el parametro especifico !!");
+            }
+        }
+    }
+}
